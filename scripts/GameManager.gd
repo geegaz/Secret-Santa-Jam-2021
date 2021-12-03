9546bbf4cedcdfe5d834540ecc35a382bf85c64b
Geegaz <geegazrd@gmail.com>
2021-12-03 01:28:48 +0100

    added dialogue box

--- scripts/GameManager.gd
+++ scripts/GameManager.gd
@@ -2,6 +2,12 @@ extends Node
 
 const SAVE_PATH := "res://save.json"
 
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_fullscreen"):
+		OS.window_fullscreen = !OS.window_fullscreen
+
 ################ Utility Functions ################
 
 # Assumes data is a JSON-compatible object
